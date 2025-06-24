ac1f4cd785e07c6699c925ebe9552d39587e77e8
Nicolas Esseiva <nicolas.esseiva@gmail.com>
2025-06-24 14:33:47 +0200

    misc: added comments

--- Patches/StartWithExtras.cs
+++ Patches/StartWithExtras.cs
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2025 Esseiva Nicolas
+ *
+ * Author: Esseiva Nicolas
+ * Date  : 06.2025
+ *
+ * Description:
+ * Patch to auto-unlock teleporter items when starting a new game.
+ * 
+ * Source : 
+ * This work uses code from this repository, under the MIT license.
+ * https://github.com/Lomeli12/StartingTeleporter
+ */
+
 using EasyTextEffects.Editor.MyBoxCopy.Extensions;
 using GameNetcodeStuff;
 using HarmonyLib;
@@ -7,6 +21,9 @@ using UnityEngine.Video;
 
 namespace MyFirstMod.Patches;
 
+/// <summary>
+/// Patch to automatically unlock teleporter items on new game start
+/// </summary>
 [HarmonyPatch(typeof(PlayerControllerB))]
 public class StartWithExtras
 {
@@ -14,19 +31,20 @@ public class StartWithExtras
     private static readonly string INVERSE_NAME = "Inverse Teleporter";
     private static int teleporterID = -1;
     private static int inverseTeleporterID = -1;
-
     private static bool idsInitialized;
     private static bool stLoaded;
 
+    /// <summary>
+    /// Find and cache teleporter item IDs from the unlockables list
+    /// </summary>
     private static void getTeleporterIDs(StartOfRound instance)
     {
         MyFirstMod.Logger.LogInfo("Getting item IDs");
         for (var i = 0; i < instance.unlockablesList.unlockables.Count; i++)
         {
             var unlockableItem = instance.unlockablesList.unlockables[i];
-            // I really hate this, cause it's a localized name. Hopefully this gets changed to proper item ids
+            // Using localized names - not ideal but necessary
             var itemName = unlockableItem.unlockableName.ToLower();
-
             if (itemName.Equals(TELEPORTER_NAME.ToLower()))
             {
                 MyFirstMod.Logger.LogInfo($"{TELEPORTER_NAME} ID: {i}");
@@ -38,34 +56,35 @@ public class StartWithExtras
                 inverseTeleporterID = i;
             }
         }
-
         idsInitialized = true;
     }
 
+    /// <summary>
+    /// Called when player connects - unlocks teleporter on new games
+    /// </summary>
     [HarmonyPatch(nameof(PlayerControllerB.ConnectClientToPlayerObject))]
     [HarmonyPostfix]
     private static void ConnectClientToPlayerObjectPatch(PlayerControllerB __instance)
     {
         StartOfRound startOfRound = StartOfRound.Instance;
-
         MyFirstMod.Logger.LogDebug("ConnectClientToPlayerObjectPatch() called");
-        //__instance.UnlockShipObject();
-
+        
         MyFirstMod.Logger.LogDebug(
             $"daysPlayersSurvivedInARow={startOfRound.daysPlayersSurvivedInARow}"
         );
-
+        
+        // Only unlock on fresh games (0 days survived)
         if (startOfRound.daysPlayersSurvivedInARow == 0)
         {
             MyFirstMod.Logger.LogDebug("New game detected - Auto-unlocking items");
-
+            
+            // Only host/server can unlock items
             if (startOfRound.NetworkManager.IsHost || startOfRound.NetworkManager.IsServer)
             {
                 if (!idsInitialized)
                 {
                     getTeleporterIDs(startOfRound);
                 }
-
                 MyFirstMod.Logger.LogDebug("Unlocking teleporter...");
                 startOfRound.BuyShipUnlockableServerRpc(teleporterID, -1);
             }
